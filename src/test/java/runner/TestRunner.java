bded0638971ca36efac0cebad0c19dad6353eec9
Jordan J. Carrillo Solis <jordan.jcarrillo.s@gmail.com>
2021-10-30 02:22:55 -0500

    Working with multiple tags - NOT expression

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions",
-			tags = "@smokeTest or @RegressionTest")
+			tags = "not @smokeTest")
 
 public class TestRunner {
 
