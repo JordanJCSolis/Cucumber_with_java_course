58c3ffaddf30cd2b4477f3c7470cd2fc35427122
Jordan J. Carrillo Solis <jordan.jcarrillo.s@gmail.com>
2021-10-29 22:30:59 -0500

    Starting project. Adding .gitignore file

---
+++ src/main/java/cucumber/example/CucumberSimpleCode/App.java
@@ -0,0 +1,13 @@
+package cucumber.example.CucumberSimpleCode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
