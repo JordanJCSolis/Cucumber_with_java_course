3462dfe2b606ba49bd0505a17913c3cb4e9ad13b
Jordan J. Carrillo Solis <jordan.jcarrillo.s@gmail.com>
2021-11-01 13:10:10 -0600

    Working with Hooks - Conditional hooks

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions",
-			tags = "@RegressionTest")
+			tags = "@RegressionTest or @SanityTest")
 
 public class TestRunner {
 
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -4,8 +4,8 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
-	
-	@Before
+
+	@Before(order = 1)
 	public void doSomethingBefore() {
 		System.out.println("Before hook instruction");
 	}
@@ -14,4 +14,15 @@ public class Hooks {
 	public void doSomethingAfter() {
 		System.out.println("After hook instruction");
 	}
+
+	@Before("@SanityTest")
+	public void conditionalBefore() {
+		System.out.println("Conditional before hook for @SanityTest tag");
+	}
+
+	@After(order = 2, value = "@SanityTest")
+	public void conditionalAfter() {
+		System.out.println("Conditional after hook for @SanityTest tag");
+	}
+
 }
